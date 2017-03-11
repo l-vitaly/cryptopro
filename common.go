767a36bff8f415be37f5cc0f5ce8d7c203743b8f
Vitaly Lobchuk <vitaly@lobchuk.ru>
2017-03-11 11:34:47 +0300

    feat(common.go): add amd64 lib path

--- common.go
+++ common.go
@@ -1,7 +1,8 @@
 package cryptopro
 
 //#cgo linux,386 darwin CFLAGS: -I/opt/cprocsp/include/cpcsp
-//#cgo linux,amd64,386 darwin LDFLAGS: -L/opt/cprocsp/lib/ -lcapi10 -lcapi20 -lrdrsup -lssp
+//#cgo linux,386 darwin LDFLAGS: -L/opt/cprocsp/lib -lcapi10 -lcapi20 -lrdrsup -lssp
+//#cgo linux,amd64 LDFLAGS: -L/opt/cprocsp/lib/amd64 -lcapi10 -lcapi20 -lrdrsup -lssp
 //#cgo linux,386 LDFLAGS: -L/opt/cprocsp/lib/ia32/ -lcapi10 -lcapi20 -lrdrsup -lssp
 //#cgo windows LDFLAGS: -lcrypt32 -lpthread
 //#include "common.h"
@@ -14,7 +15,7 @@ import (
 
 var (
 	ErrCreatingCertificateCtx = errors.New("error a new certificate could not be created")
-    ErrVerifyingSignature = errors.New("error verifying message signature")
+	ErrVerifyingSignature     = errors.New("error verifying message signature")
 )
 
 func charPtr(s string) *C.CHAR {
